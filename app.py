08fc1e0ceea9865be4be37f434dd894f8c923283
zoserme <149444912+zoserme@users.noreply.github.com>
2023-11-20 23:55:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import time
 st.title('Datos Hidrometereológicos Gobierno Regional Piura')
