ceba659f7174b7efb8c7184ba71c9400701d703e
zoserme <149444912+zoserme@users.noreply.github.com>
2023-11-20 23:29:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import time
+st.title('Datos Hidrometereológicos Gobierno Regional Piura')
+df = pd.read_csv('tb_medida_estaciones (1)_0.csv')
+# Datos Hidrometereológicos Gobierno Regional Piura
+st.title("Dashboard de Análisis de Datos")
+# Mostrar el dataset en una tabla
+st.subheader("Dataset")
+st.dataframe(df)
