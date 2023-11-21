5c6e8365812d995b77dbccfe3453e09d7681d01a
zoserme <149444912+zoserme@users.noreply.github.com>
2023-11-21 00:10:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import pandas as pd
 #import seaborn as sns
 #from datetime import time
 st.title('Datos Hidrometereológicos Gobierno Regional Piura')
-#option = st.selectbox(
-#'¿Cómo desearía ser contactado/a?',
-#('Email', 'Teléfono', 'Whatsapp'))
-#st.write('Seleccionó:', option)
 df = pd.read_csv('tb_medida_estaciones (1)_0.csv')
 # Datos Hidrometereológicos Gobierno Regional Piura
 st.title("Dashboard de Análisis de Datos")
 # Mostrar el dataset en una tabla
 st.subheader("Dataset")
 st.dataframe(df)
+# Mostrar un gráfico de línea con las columnas 13 y 14
+st.subheader("Gráfico de Columnas 13 y 14")
+st.line_chart(df[[13, 14]])
