1f89b0dabdd475663cc07a8b5c11365627b3fb67
zoserme <149444912+zoserme@users.noreply.github.com>
2023-11-21 00:03:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pandas as pd
 #import seaborn as sns
 #from datetime import time
 st.title('Datos Hidrometereológicos Gobierno Regional Piura')
-option = st.selectbox(
-'¿Cómo desearía ser contactado/a?',
-('Email', 'Teléfono', 'Whatsapp'))
-st.write('Seleccionó:', option)
-#df = pd.read_csv('tb_medida_estaciones (1)_0.csv')
+#option = st.selectbox(
+#'¿Cómo desearía ser contactado/a?',
+#('Email', 'Teléfono', 'Whatsapp'))
+#st.write('Seleccionó:', option)
+df = pd.read_csv('tb_medida_estaciones (1)_0.csv')
 # Datos Hidrometereológicos Gobierno Regional Piura
-#st.title("Dashboard de Análisis de Datos")
+st.title("Dashboard de Análisis de Datos")
 # Mostrar el dataset en una tabla
-#st.subheader("Dataset")
-#st.dataframe(df)
+st.subheader("Dataset")
+st.dataframe(df)
